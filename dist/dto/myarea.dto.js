e62ff7c6deba06197f7a9f05d6b6a7949e095514
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-08-20 19:43:47 +0300

    Create myarea.dto.js

---
+++ dist/dto/myarea.dto.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyAreaDto = void 0;
+class MyAreaDto {
+}
+exports.MyAreaDto = MyAreaDto;
+//# sourceMappingURL=myarea.dto.js.map
